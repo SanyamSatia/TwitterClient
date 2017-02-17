36c110cb4ec887d861e5d3c2b889f2d50137118b
7Aces <sanyam.satia@ymail.com>
2017-02-17 12:52:08 -0600

    Add models

---
+++ TwitterDemo/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  TwitterDemo
+//
+//  Created by Sanyam Satia on 2/17/17.
+//  Copyright © 2017 Sanyam Satia. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ TwitterDemo/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  TwitterDemo
+//
+//  Created by Sanyam Satia on 2/17/17.
+//  Copyright © 2017 Sanyam Satia. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1SessionManager {
+
+}
